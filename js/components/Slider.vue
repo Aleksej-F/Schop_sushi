210e7ff1e58b292bd0c995b99bc7a995f31fd651
AlesejF <l7933@yandex.ru>
2021-05-26 01:52:52 +0300

    доделал слайдер

--- js/components/Slider.vue
+++ js/components/Slider.vue
@@ -7,7 +7,7 @@
             
         >
             <div class="slider-item" data-n="0"
-                v-bind:style="  leftSlaid.a "
+                v-bind:style="leftSlaid.a"
             >
                 <div class="slider-item-link">
                     <a href="">
@@ -17,7 +17,7 @@
             </div>
 
             <div class="slider-item" data-n="1"
-                v-bind:style="leftSlaid.b "
+                v-bind:style="leftSlaid.b"
             >
                 <div class="slider-item-link">
                     <a href="">
@@ -27,7 +27,7 @@
             </div>
 
             <div class="slider-item" data-n="2"
-                v-bind:style="leftSlaid.c "
+                v-bind:style="leftSlaid.c"
             >
                 <div class="slider-item-link"></div>
             </div>
@@ -61,37 +61,49 @@ export default {
             changeLeft:0,
             leftDown:0,
             activeNav:[{id:0, q:true},{id:1,q:false},{id:2,q:false}],
-            activeNavC: 0
+            activeNavC: 0,
+            slidePosition:[]  
         }
     },
     mounted() {
         this.widthSlide = window.innerWidth;
         this.leftSlaidNull = {a: 0, b: this.widthSlide, c: -this.widthSlide};
         this.leftSlaid = {a:{left: 0 + 'px'}, b:{left: this.widthSlide + 'px'}, c:{left: -this.widthSlide + 'px'}};
-
+        this.slidePosition = [
+            [0,this.widthSlide,-this.widthSlide],
+            [-this.widthSlide,0,this.widthSlide],
+            [this.widthSlide,-this.widthSlide,0]
+        ]
     },
 
     methods: {
         down(e)  {
-            console.log('нажал кнопку мыши')           
-            console.log(window.innerWidth)
-            this.downPrizn=true
-            this.leftDown=e.offsetX
-            console.log(this.leftDown)
-            this.leftSlaid.a={
-                        left : this.leftSlaidNull.a  +  'px',
-                        transition: ''
-            }
-            this.leftSlaid.b={
-                        left : this.leftSlaidNull.b  +  'px',
-                        transition: ''
-            }
-            this.leftSlaid.c={
-                        left :this.leftSlaidNull.c  +  'px',
-                        transition: ''
-            }
+            
+            //console.log(e)  
 
+           // console.log(window.innerWidth)
+            
+                console.log('нажал кнопку мыши')  
+                if (e !== undefined){
+                    this.downPrizn = true
+                    this.leftDown=e.offsetX
+                    console.log(this.leftDown)
+                }
 
+                this.leftSlaid.a={
+                            left : this.leftSlaidNull.a  +  'px',
+                            transition: ''
+                }
+                this.leftSlaid.b={
+                            left : this.leftSlaidNull.b  +  'px',
+                            transition: ''
+                }
+                this.leftSlaid.c={
+                            left :this.leftSlaidNull.c  +  'px',
+                            transition: ''
+                }
+
+           
 
         },
 
@@ -99,14 +111,15 @@ export default {
             console.log('отпустил кнопку мыши')  
             this.downPrizn=false
             
-            if (Math.abs(this.changeLeft)>100) {
+            if (Math.abs(this.changeLeft)>150) {
                  console.log('this.changeLeft')
                  console.log(this.changeLeft)
                  this.activeNav[this.activeNavC].q = !this.activeNav[this.activeNavC].q
          
                 if (this.changeLeft < 0) {
                     console.log('leftSlaid < 0') 
-                     if (this.activeNavC === this.activeNav.length-1) {
+
+                    if (this.activeNavC === this.activeNav.length-1) {
                         this.activeNavC = 0
                     } else {this.activeNavC++}
                     
@@ -130,17 +143,18 @@ export default {
                         this.leftSlaidNull.b = Number(this.leftSlaid.b.left.slice(0 , -2)) < -this.widthSlide ? this.widthSlide : Number(this.leftSlaid.b.left.slice(0 , -2))
                         this.leftSlaidNull.c = Number(this.leftSlaid.c.left.slice(0 , -2)) < -this.widthSlide ? this.widthSlide : Number(this.leftSlaid.c.left.slice(0 , -2))
                     }, 800);
-                     console.log(this.leftSlaidNull)
+
+                    
                 } else {
                     console.log('leftSlaid>0')
                    
-                     if (this.activeNavC === 0) {
+                    if (this.activeNavC === 0) {
                         this.activeNavC = this.activeNav.length-1
                     } else {this.activeNavC--}
                     
                     this.activeNav[this.activeNavC].q = !this.activeNav[this.activeNavC].q
 
-                     this.leftSlaid.a={
+                    this.leftSlaid.a={
                         left : this.leftSlaidNull.a + this.widthSlide +  'px',
                         transition: 'left .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)'
                     }
@@ -152,44 +166,112 @@ export default {
                         left :this.leftSlaidNull.c + this.widthSlide +  'px',
                         transition: 'left .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)'
                     }
-                    
-                    
+                                        
                     setTimeout(() => {
                         this.leftSlaidNull.a = Number(this.leftSlaid.a.left.slice(0 , -2)) > this.widthSlide ? -this.widthSlide : Number(this.leftSlaid.a.left.slice(0 , -2))
                         this.leftSlaidNull.b = Number(this.leftSlaid.b.left.slice(0 , -2)) > this.widthSlide ? -this.widthSlide : Number(this.leftSlaid.b.left.slice(0 , -2))
                         this.leftSlaidNull.c = Number(this.leftSlaid.c.left.slice(0 , -2)) > this.widthSlide ? -this.widthSlide : Number(this.leftSlaid.c.left.slice(0 , -2))
                        
                     }, 800);
-                    console.log(this.leftSlaidNull)
+
+                   
                 }
 
                 
             }else{
-                this.leftSlaid.a.left = this.leftSlaidNull.a +  'px'
-                this.leftSlaid.b.left = this.leftSlaidNull.b +  'px'
-                this.leftSlaid.c.left = this.leftSlaidNull.c +  'px'
+                this.leftSlaid.a={
+                    left:this.leftSlaidNull.a +  'px',
+                    transition: 'left .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)'}
+                this.leftSlaid.b={
+                    left:this.leftSlaidNull.b +  'px',
+                    transition: 'left .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)'}
+                this.leftSlaid.c={
+                    left:this.leftSlaidNull.c +  'px',
+                    transition: 'left .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)'}
 
             }
+            this.changeLeft = 0
         },
 
         mousemove(e){
-            //console.log(this.downPrizn)
-            
             if (this.downPrizn){
-               // console.log(e.offsetX)
                 this.changeLeft = e.offsetX - this.leftDown
                 this.leftSlaid.a.left = this.leftSlaidNull.a + this.changeLeft +  'px'
                 this.leftSlaid.b.left = this.leftSlaidNull.b + this.changeLeft +  'px'
                 this.leftSlaid.c.left = this.leftSlaidNull.c + this.changeLeft +  'px'
-                console.log(this.changeLeft)
-                console.log(this.leftSlaid)
             }
         },
 
         clickNav(e){
             console.dir(e.target.attributes['data-n'].textContent)
+            const aktive = e.target.attributes['data-n'].textContent
             //.attributes.data-n.textContent
-            },
+            if ( !this.activeNav[aktive].q) {  
+                switch(aktive) {
+                    case '0':
+                        console.log('нажал 0');
+                        if ( this.activeNav[1].q) {
+                            console.log('да');
+                            this.leftSlaidNull.a = this.widthSlide;
+                            this.leftSlaidNull.c = -this.widthSlide;
+                        }
+
+
+                        
+                        break;
+                    case '1':
+                        console.log('нажал 1');
+                        if ( this.activeNav[2].q) {
+                            console.log('да');
+                            this.leftSlaidNull.b = this.widthSlide;
+                            this.leftSlaidNull.a = -this.widthSlide;}
+                        break;
+                    case '2':
+                        console.log('нажал 2');
+                        if ( this.activeNav[0].q) {
+                            console.log('да');
+                            this.leftSlaidNull.c = this.widthSlide;
+                            this.leftSlaidNull.b = -this.widthSlide;
+                        }
+                        break;
+                    
+
+                }
+                
+                
+                this.activeNav[this.activeNavC].q = !this.activeNav[this.activeNavC].q
+                this.activeNavC = Number(aktive)
+                this.activeNav[this.activeNavC].q = !this.activeNav[this.activeNavC].q
+                console.log(this.leftSlaid)
+                   
+                
+                const p =  new Promise((resolve)=>{
+			        this.down()
+				    setTimeout(() => {resolve()}, 100)
+			
+		        })
+                p.then(()=>{ 
+                    this.leftSlaid.a={
+                        left : this.leftSlaidNull.a - this.widthSlide +  'px',
+                        transition: 'left .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)'
+                    }
+                    this.leftSlaid.b={
+                        left : this.leftSlaidNull.b - this.widthSlide +  'px',
+                        transition: 'left .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)'
+                    }
+                    this.leftSlaid.c={
+                        left : this.leftSlaidNull.c - this.widthSlide +  'px',
+                        transition: 'left .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)'
+                    }
+
+                     setTimeout(() => {
+                        this.leftSlaidNull.a = this.slidePosition[aktive][0]
+                        this.leftSlaidNull.b = this.slidePosition[aktive][1]
+                        this.leftSlaidNull.c = this.slidePosition[aktive][2]
+                    }, 800);
+                })
+            }
+        },
         
     },
 }
